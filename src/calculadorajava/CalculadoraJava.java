29fa17eb1531d3bdb531b5d9763f366049f9218d
Gladiston Fernando <Gladiston Fernando@DESKTOP-EEF4JNS>
2017-06-10 16:47:32 -0300

    calculadora v2.0

--- src/calculadorajava/CalculadoraJava.java
+++ src/calculadorajava/CalculadoraJava.java
@@ -15,7 +15,10 @@ public class CalculadoraJava {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       Divisao d =new Divisao();
+       Subitracao s =new Subitracao();
+       d.divisao(20, 5);
+       s.subitracao(20, 10);
     }
     
 }
